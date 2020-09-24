819734a0d108d76aba89a62752dbb9c9a3bfb101
Jaysal Patel <62490579+jaysalpatel@users.noreply.github.com>
2020-09-24 13:59:30 -0500

    created backend lambda function

---
+++ counter.py
@@ -0,0 +1,20 @@
+from boto3 import resource
+
+connection = resource('dynamodb', region='us-east-1')
+table = connection.Table('visitorcounter')
+
+def visitorcounter(event, context):
+    update = table.get_item(Key={'counter': '1'})
+    update.get('Item') or table.put_item(Item={'counter': '1', 'visitorcount': '0'})
+    
+    item = table.get_item(Key={'counter': '1'}).get('Item')
+    
+    count = item.get('visitorcount') + 1
+    table.put_item('counter': '1'. 'visitorcount' : count})
+    return {
+        'statusCode': 200.
+        'body': count,
+        'headers': {
+            'Access-Control-Allow-Origin': '*'
+        }
+ }
