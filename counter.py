a34477f25444efc9dd6fb665f3ca078d6b9ce9e5
Jaysal Patel <62490579+jaysalpatel@users.noreply.github.com>
2020-09-24 14:10:55 -0500

    Update counter.py

--- counter.py
+++ counter.py
@@ -3,7 +3,7 @@ from boto3 import resource
 connection = resource('dynamodb', region='us-east-1')
 table = connection.Table('visitorcounter')
 
-def visitorcounter(event, context):
+def lambda_handler(event, context):
     update = table.get_item(Key={'counter': '1'})
     update.get('Item') or table.put_item(Item={'counter': '1', 'visitorcount': '0'})
     
@@ -12,7 +12,7 @@ def visitorcounter(event, context):
     count = item.get('visitorcount') + 1
     table.put_item('counter': '1'. 'visitorcount' : count})
     return {
-        'statusCode': 200.
+        'statusCode': 200,
         'body': count,
         'headers': {
             'Access-Control-Allow-Origin': '*'
