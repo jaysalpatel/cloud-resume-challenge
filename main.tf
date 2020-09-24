f9bcd52f730d880e9753d0db0947dcb59e72f46d
Jaysal Patel <62490579+jaysalpatel@users.noreply.github.com>
2020-09-24 14:11:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_s3_bucket" "cloud-resume-challenge-jaysalpatel" {
 
 resource "aws_dynamodb_table" "visitorcounter" {
     name = "visitorcounter"
-    billing_mode = "PROVISIONED"
+    billing_mode = "PAY_PER_REQUEST"
     read_capacity = 5
     write_capacity = 5
     hash_key = "visitorcounter"
